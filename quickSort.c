126bfc93f578ec1865023d7001eabf34911ccfff
Steve <sayantanmanna06@gmail.com>
2023-08-23 01:25:18 +0530

    added different flavors of quicksort

--- quickSort.c
+++ quickSort.c
@@ -49,6 +49,70 @@
         sort(nums, e+1, hi);
     }
 
+    void quickSort(int[] arr, int low, int high){   
+
+        // Here, we are choosing the high element as pivot and placing it at its correct position
+        
+         if (low < high) {
+           int i = low;
+           int pivot = arr[high];
+
+            for (int j = low; j < high; j++) {
+                // i always points to the element grater than the pivot, dry run this part for better understanding
+                if (arr[j] <= pivot) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                }
+            }
+            // swapping the element at 'i' with the pivot element
+            int temp = arr[i];
+            arr[i] = arr[high];
+            arr[high] = temp; 
+            
+            // now my pivot is at index i, sort the two sides of pivot
+            quickSort(arr, low, i - 1);
+            quickSort(arr, i + 1, high);
+        }
+    }
+
+
+    void quickSort2 (int[] arr, int low, int high){   
+
+        // Here, we are choosing the mid element as pivot and placing it at its correct position
+        
+        if (low < high) {
+            int i = low;
+            int mid= low+(high-low)/2;
+            int pivot = arr[mid];
+
+        // Sending the pivot element to high to avoid conflict
+
+            int temp1 = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = temp1;
+
+        // Now, our pivot is located at high, hence the remaining part is same as the previous   
+            for (int j = low; j < high; j++) {
+                if (arr[j] <= pivot) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                }
+            }
+        
+            int temp = arr[i];
+            arr[i] = arr[high];
+            arr[high] = temp; 
+            
+            quickSort2 (arr, low, i - 1);
+            quickSort2 (arr, i + 1, high);
+        }
+    }
+
+
 
  int main(){
     int arr[]={22,34,56,12,6};
